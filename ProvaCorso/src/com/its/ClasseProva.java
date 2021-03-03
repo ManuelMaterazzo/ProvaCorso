802293ad1eedce52f7d2bc11bf04069f91ce4384
Manuel Materazzo <manuel.materazzo@stud.tecnicosuperiorekennedy.it>
2021-03-03 10:12:05 +0100

    forth commit

--- ProvaCorso/src/com/its/ClasseProva.java
+++ ProvaCorso/src/com/its/ClasseProva.java
@@ -3,6 +3,6 @@ package com.its;
 public class ClasseProva {
 	public static void main(String[] args) {
 		System.out.println("Ciao");
-		System.out.println("ok");
+		System.out.println("Sei sus");
 	}
 }
